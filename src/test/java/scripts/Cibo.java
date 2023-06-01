6f79a58449981ba0b0b59d28b606cd4805577a5d
LaluPujari <pujarilaluskl@gmail.com>
2023-06-01 13:11:58 +0530

    4th commit msg

--- src/test/java/scripts/Cibo.java
+++ src/test/java/scripts/Cibo.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.firefox.FirefoxDriver;
 public class Cibo {
 	
 	
-	// master
+	//master to local 
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
